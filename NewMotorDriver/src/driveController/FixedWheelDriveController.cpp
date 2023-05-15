47e953e5e506b02be6e09eae1ecd14969859a073
ModellbahnFreak <freak.mdellbahn@gmail.com>
2023-05-15 18:44:01 +0200

    Started implementation of fixed wheel drive controller

---
+++ NewMotorDriver/src/driveController/FixedWheelDriveController.cpp
@@ -0,0 +1,50 @@
+#include "FixedWheelDriveController.hpp"
+
+FixedWheelDriveController::FixedWheelDriveController(
+    Motor &frontLeftMotor, Motor &frontRightMotor,
+    Motor &rearLeftMotor, Motor &rearRightMotor,
+    uint8_t wheelDistanceWidthMm, uint8_t wheelDistanceLengthMm)
+    : frontLeft(frontLeftMotor), frontRight(frontRightMotor),
+      rearLeft(rearLeftMotor), rearRight(rearRightMotor),
+      carWidthMm(wheelDistanceWidthMm), carLengthMm(wheelDistanceLengthMm)
+{
+}
+
+void FixedWheelDriveController::setSpeedInternal(int16_t speed)
+{
+    if (this->angle == 0)
+    {
+        this->rearLeft.setSpeed(speed);
+        this->rearRight.setSpeed(speed);
+        return;
+    }
+
+    float innerRadius = tan((90.0 - abs(this->angle)) * PI / 180.0) * this->carLengthMm;
+    float middleRadius = innerRadius + this->carWidthMm / 2;
+    float outerWheelSpeedFactor = (innerRadius + this->carWidthMm) / middleRadius;
+    float innerWheelSpeedFactor = innerRadius / middleRadius;
+    if (this->angle > 0)
+    {
+        this->rearLeft.setSpeed(speed * outerWheelSpeedFactor);
+        this->rearRight.setSpeed(speed * innerWheelSpeedFactor);
+    }
+    else
+    {
+        this->rearLeft.setSpeed(speed * innerWheelSpeedFactor);
+        this->rearRight.setSpeed(speed * outerWheelSpeedFactor);
+    }
+}
+
+void FixedWheelDriveController::setAngleInternal(int8_t angle)
+{
+    // todo
+    this->setSpeedInternal(this->getSpeed());
+}
+
+void FixedWheelDriveController::loop()
+{
+    this->frontLeft.loop();
+    this->frontRight.loop();
+    this->rearLeft.loop();
+    this->rearRight.loop();
+}
---
+++ NewMotorDriver/src/driveController/FixedWheelDriveController.hpp
@@ -0,0 +1,31 @@
+#ifndef FIXED_WHEEL_DRIVE_CONTROLLER_HPP
+#define FIXED_WHEEL_DRIVE_CONTROLLER_HPP
+
+#include "DriveController.hpp"
+#include "../motors/Motor.hpp"
+
+class FixedWheelDriveController : public DriveController
+{
+private:
+    Motor &frontLeft;
+    Motor &frontRight;
+    Motor &rearLeft;
+    Motor &rearRight;
+    uint8_t carWidthMm;
+    uint8_t carLengthMm;
+
+public:
+    FixedWheelDriveController(
+        Motor &frontLeftMotor, Motor &frontRightMotor,
+        Motor &rearLeftMotor, Motor &rearRightMotor,
+        uint8_t wheelDistanceWidthMm, uint8_t wheelDistanceLengthMm);
+    ~FixedWheelDriveController(){};
+
+    void loop() override;
+
+protected:
+    void setSpeedInternal(int16_t speed) override;
+    void setAngleInternal(int8_t angle) override;
+};
+
+#endif
