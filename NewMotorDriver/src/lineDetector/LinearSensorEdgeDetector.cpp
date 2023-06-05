b4a7f280c5b9764f958ca45c44bab30f37dbe187
ModellbahnFreak <freak.mdellbahn@gmail.com>
2023-06-05 16:33:30 +0200

    Rewrite of edge detection

--- NewMotorDriver/src/lineDetector/LinearSensorEdgeDetector.cpp
+++ NewMotorDriver/src/lineDetector/LinearSensorEdgeDetector.cpp
@@ -2,6 +2,8 @@
 
 #define LINE_SENSOR_DEBUG
 
+#define NO_SENSOR_INDEX 255
+
 float floatMap(float val, float inMin, float inMax, float outMin, float outMax)
 {
     return (val - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
@@ -58,51 +60,110 @@ int8_t LinearSensorEdgeDetector::getLinePositionMm()
         fValues[this->leftEdge ? i : this->totalSensors - i - 1] = mapped;
     }
 #ifdef LINE_SENSOR_DEBUG
-    Serial.println();
+    // Serial.println();
 #endif
 
-    uint8_t firstNonWhite = 255;
-    uint8_t firstBlackIndex = 255;
+    uint8_t firstNonWhite = NO_SENSOR_INDEX;
+    uint8_t firstBlackIndex = NO_SENSOR_INDEX;
     for (uint8_t i = 0; i < this->totalSensors; i++)
     {
-        if (firstBlackIndex == 255 && fValues[i] >= 1)
+        if (firstBlackIndex == NO_SENSOR_INDEX && fValues[i] >= 1)
         {
             firstBlackIndex = i;
         }
-        if (firstNonWhite == 255 && fValues[i] > 0)
+        if (firstNonWhite == NO_SENSOR_INDEX && fValues[i] > 0.1)
         {
             firstNonWhite = i;
         }
     }
 
+#ifdef LINE_SENSOR_DEBUG
+    Serial.print("; First non white: ");
+    Serial.print(firstNonWhite);
+    Serial.print("; First black: ");
+    Serial.print(firstBlackIndex);
+#endif
+
     float linePosRel = NAN;
     if (firstBlackIndex == firstNonWhite + 1)
     {
-        linePosRel = firstBlackIndex - fValues[firstNonWhite] - (this->totalSensors * 0.5);
+        Serial.print("; A");
+        // edge between first non white and first black
+        linePosRel = firstBlackIndex - fValues[firstNonWhite];
     }
-    else if (firstNonWhite != 255 && firstNonWhite < this->totalSensors - 1)
+    else if (firstNonWhite != NO_SENSOR_INDEX && firstBlackIndex != NO_SENSOR_INDEX)
     {
+        if (firstBlackIndex > 0)
+        {
+            if (firstNonWhite == firstBlackIndex)
+            {
+                Serial.print("; B");
+                // edge is (somewhere) between first black and previous
+                linePosRel = firstBlackIndex - 0.5;
+            }
+            else
+            {
+                Serial.print("; C");
+                // edge between first black and one previous
+                linePosRel = firstBlackIndex - fValues[firstNonWhite];
+            }
+        }
+        else
+        {
+            Serial.print("; D");
+            // black is at edge of sensor array; edge of line outside
+            linePosRel = 0;
+        }
+    }
+    else if (firstNonWhite != NO_SENSOR_INDEX && firstBlackIndex == NO_SENSOR_INDEX)
+    {
+        // no black found; edge either between first non white and next one or previous one (depending on value of next one)
         float firstNonWhiteValue = fValues[firstNonWhite];
-        float nextValue = fValues[firstNonWhite + 1];
-        float scale = 1.0 / (firstNonWhiteValue + nextValue);
-        firstNonWhiteValue *= scale;
-        nextValue *= scale;
-        float posBetween = (-firstNonWhiteValue + nextValue) / 2.0 + 1;
-        linePosRel = firstNonWhite + posBetween - (this->totalSensors * 0.5);
-    }
-    else if (firstNonWhite != 255)
-    {
-        linePosRel = this->totalSensors * 0.5;
+        float nextValue = firstNonWhite < this->totalSensors - 1 ? fValues[firstNonWhite + 1] : 0;
+
+        if (nextValue > 0)
+        {
+            Serial.print("; E");
+            // sensor after first non white is darker => line edge must be between that and the first non white
+            float scale = 1.0 / (firstNonWhiteValue + nextValue);
+            firstNonWhiteValue *= scale;
+            nextValue *= scale;
+            float posBetween = (-firstNonWhiteValue + nextValue) / 2.0 + 0.5;
+            linePosRel = firstNonWhite + posBetween;
+        }
+        else
+        {
+            if (firstNonWhite > 0)
+            {
+                Serial.print("; F");
+                // sensor after first non white is white (as is the one previous) => line edge must be between previous and first non white
+                linePosRel = firstNonWhite - 0.5;
+            }
+            else
+            {
+                Serial.print("; G");
+                // sensor after first non white is white and first non white is the first sensor => line edge is propably outside
+                linePosRel = 0;
+            }
+        }
     }
 
     if (linePosRel != NAN)
     {
+        linePosRel -= (this->totalSensors - 1) * 0.5;
         linePosRel = this->leftEdge ? linePosRel : -linePosRel;
+#ifdef LINE_SENSOR_DEBUG
+        Serial.print("; Detected pos: ");
+        Serial.println(linePosRel);
+#endif
         int8_t pos = round(linePosRel * this->sensorDistance);
         return pos;
     }
     else
     {
+#ifdef LINE_SENSOR_DEBUG
+        Serial.println("; Detected pos: nan");
+#endif
         return LINE_DETECTOR_NO_LINE_FOUND;
     }
 }
