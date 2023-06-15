59bcee3799580065a0da191e4d6c560511d7bce0
ModellbahnFreak <freak.mdellbahn@gmail.com>
2023-06-15 12:28:39 +0200

    Added lazyer sttering alg

--- NewMotorDriver/lineFollowing.cpp
+++ NewMotorDriver/lineFollowing.cpp
@@ -1,6 +1,16 @@
 #include "Arduino.h"
 #include "lineFollowing.hpp"
 
+// #define TIMED_STEERING
+
+#ifdef TIMED_STEERING
+#define MS_UNTIL_MAX_ANGLE 1000
+#define MIN_ANGLE 5
+#define MAX_ANGLE 25
+float lastAngle = 0;
+unsigned long lastTime = millis();
+#endif
+
 void lineFollowing(DriveController &dc, LineDetector &ld, DistanceSensor &distFront, DistanceSensor &distRear)
 {
     uint16_t d = distFront.getDistanceToClosestMm();
@@ -28,8 +38,37 @@ void lineFollowing(DriveController &dc, LineDetector &ld, DistanceSensor &distFr
         Serial.print("Setting angle to ");
         Serial.println(angle);
         dc.setAngle(angle);
+#elif defined(TIMED_STEERING)
+        unsigned long thisTime = millis();
+        float newAngle = lastAngle;
+        if ((lastAngle < 0 && lp >= 0) || (lastAngle > 0 && lp <= 0))
+        {
+            newAngle = 0;
+        }
+        else
+        {
+            lastAngle = abs(lastAngle);
+            float dstAngle = abs(lp) * MAX_ANGLE / 16;
+            if (dstAngle > lastAngle)
+            {
+                float incAngle = (thisTime - lastTime) * dstAngle / MS_UNTIL_MAX_ANGLE;
+                newAngle = lastAngle + incAngle;
+            }
+            else
+            {
+                newAngle = dstAngle;
+            }
+            newAngle = min(MAX_ANGLE, newAngle);
+            if (lp < 0)
+            {
+                newAngle = -newAngle;
+            }
+        }
+        dc.setAngle(newAngle);
+        lastAngle = newAngle;
+        lastTime = thisTime;
 #else
-        dc.setAngle(lp * 30 / 16);
+        // dc.setAngle(lp * 20 / 16);
 #endif
     }
 }
