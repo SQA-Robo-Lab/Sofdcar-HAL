b4013b72c50385aaeac1a0b88a9b371f7331fb84
ModellbahnFreak <freak.mdellbahn@gmail.com>
2023-07-10 12:09:22 +0200

    Completed motor calibrator script

--- examples/MotorCalibrator/MotorCalibrator.ino
+++ examples/MotorCalibrator/MotorCalibrator.ino
@@ -7,17 +7,28 @@ unsigned long times[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 uint8_t readSpeed = 0;
 
-uint16_t currStep = 0;
-unsigned long nextStepAt = 0;
+int16_t currStep = 0;
+int16_t pausedSpeed = 0;
+unsigned long lastStepAt = 0;
 
 MotorDcHBridge frontLeft(MOTOR_SPEED_UNIT_RPM, 2, 4, 3);
 MotorDcHBridge frontRight(MOTOR_SPEED_UNIT_RPM, 7, 5, 6);
 MotorDcHBridge rearLeft(MOTOR_SPEED_UNIT_RPM, 8, 10, 9);
 MotorDcHBridge rearRight(MOTOR_SPEED_UNIT_RPM, 13, 11, 12);
 
+int16_t rpmLut[256 * 2];
+
 void onFree()
 {
     counts[0]++;
+}
+
+void setSpeed(int32_t speed)
+{
+    frontLeft.setSpeed(speed);
+    frontRight.setSpeed(speed);
+    rearLeft.setSpeed(speed);
+    rearRight.setSpeed(speed);
 }
 
 void setup()
@@ -44,9 +55,12 @@ void loop()
         char read = Serial.read();
         if (read == '\n')
         {
-            Serial.print("Setting to ");
-            Serial.println(readSpeed);
-            frontRight.setSpeed(readSpeed);
+            if (currStep == 0)
+            {
+                Serial.print("Setting to ");
+                Serial.println(readSpeed);
+                setSpeed(readSpeed);
+            }
             readSpeed = 0;
         }
         else if (read >= '0' && read <= '9')
@@ -56,9 +70,34 @@ void loop()
         else if (read == 't')
         {
             Serial.println("Starting automatic test");
+            pausedSpeed = 0;
             currStep = 255;
-            frontRight.setSpeed(currStep);
-            delay(1000);
+            setSpeed(currStep);
+            lastStepAt = micros() + 1000000;
+        }
+        else if (read == 'c')
+        {
+            currStep = 0;
+            pausedSpeed = 0;
+            setSpeed(currStep);
+            Serial.println("Cancelled test");
+        }
+        else if (read == 'p')
+        {
+            pausedSpeed = currStep;
+            currStep = 0;
+            setSpeed(currStep);
+            Serial.print("Paused automatic test at speed ");
+            Serial.println(pausedSpeed);
+        }
+        else if (read == 'r')
+        {
+            Serial.print("Resuming automatic test at speed ");
+            Serial.println(pausedSpeed);
+            currStep = pausedSpeed;
+            pausedSpeed = 0;
+            setSpeed(currStep);
+            lastStepAt = micros() + 1000000;
         }
     }
 
@@ -81,29 +120,58 @@ void loop()
     }
     cpus = cpus / (now - last);
 
-    if (currStep > 0 && currStep < 256)
+    if (currStep != 0)
     {
-        if ((now - nextStepAt) >= 500000)
+        unsigned long passed = now - lastStepAt;
+        if (passed >= 500000 && passed < __LONG_MAX__)
         {
-            frontRight.setSpeed(currStep);
-            nextStepAt = now;
+            int16_t lastStep = currStep;
+            lastStepAt = now;
+            if (currStep > 0)
+            {
+                currStep--;
+                if (currStep == 0)
+                {
+                    lastStepAt = now + 1000000;
+                    currStep = -256;
+                }
+            }
+            else
+            {
+                currStep++;
+                if (currStep == 0)
+                {
+
+                    Serial.println("Test result: ");
+                    Serial.print("{");
+                    for (uint16_t i = 0; i < 2 * 256; i++)
+                    {
+                        Serial.print(rpmLut[i]);
+                        Serial.print(", ");
+                    }
+                    Serial.println("}");
+                }
+            }
+            setSpeed(currStep);
+
+            float rpm = cpus * 60000000.0 / 20;
+            rpmLut[lastStep + 256] = floor(rpm * (lastStep < 0 ? -1 : 1));
             Serial.print("At speed ");
-            Serial.print(currStep);
+            Serial.print(lastStep);
             Serial.print(", ");
-            Serial.print(cpus * 60000000.0 / 20);
+            Serial.print(rpmLut[lastStep + 256]);
             Serial.println(" rpm");
-            currStep--;
         }
     }
     else
     {
-        currStep = 0;
+        /*currStep = 0;
         Serial.print("Pulses per ms: ");
         Serial.print(cpus);
         Serial.print(", Pulses per s: ");
         Serial.print(cpus * 1000000.0);
         Serial.print(", Rpm: ");
-        Serial.println(cpus * 60000000.0 / 20);
+        Serial.println(cpus * 60000000.0 / 20);*/
     }
 
     delay(50);
