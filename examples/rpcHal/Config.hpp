ca547a067bb5a539e1e4e9083bcecf50d142fa5a
ModellbahnFreak <freak.mdellbahn@gmail.com>
2024-04-10 18:16:09 +0200

    more complete example sketch

---
+++ examples/rpcHal/Config.hpp
@@ -0,0 +1,1 @@
+../SimpleHardwareController/Config.hpp
--- examples/rpcHal/rpcHal.ino
+++ examples/rpcHal/rpcHal.ino
@@ -1,13 +1,36 @@
 #include <Sofdcar-HAL.hpp>
+#include <SimpleHardwareController.hpp>
+#include <SimpleHardwareController_Connector.h>
+#include "Config.hpp"
 #include "Arduino.h"
 
-RpcManager mgr(Serial, rpc_createForHal());
+SimpleHardwareController *controller = nullptr;
+
+RpcClass *rootClass = nullptr;
+RpcManager *mgr = nullptr;
 
 void setup()
 {
+    Serial.begin(115200);
+    Serial.println("Starting");
+
+    rootClass = rpc_createForHal();
+
+    mgr = new RpcManager(Serial, rootClass);
+
+    controller = new SimpleHardwareController;
+
+    initSofdcarHalConnectorFor(controller);
+    controller->initializeCar(config, lineConfig);
+
+    ((RpcRootMember *)rootClass->getMember("dc"))->updateValue(controller->getDriveController());
+
+    delay(100);
+    Serial.println("Started");
 }
 
 void loop()
 {
-    mgr.loop();
+    mgr->loop();
+    delay(100);
 }
